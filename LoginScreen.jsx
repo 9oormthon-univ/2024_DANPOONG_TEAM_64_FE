4c0dfa16febaa7ee30e4019d7c28c21e73a2e244
chan8632 <duscks8632@gmail.com>
2024-11-24 01:34:45 +0900

    안드로이드, jdk, keytool 설치 및 카카오 설정

--- LoginScreen.jsx
+++ LoginScreen.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacityButton,
+  Button,
+} from "react-native";
 import TitleImage from "./assets/로그인/주식 한입.svg";
 import KakakoImage from "./assets/로그인/카카오톡.svg";
-
+import { login, logout, getProfile } from "@react-native-seoul/kakao-login";
 export default function LoginScreen({ navigation }) {
+  const handleLogin = async () => {
+    try {
+      const result = await login();
+      console.log("로그인 성공:", result);
+      const profile = await getProfile();
+      console.log("프로필 정보:", profile);
+    } catch (error) {
+      console.error("로그인 실패:", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      Alert.alert("로그아웃 완료");
+    } catch (error) {
+      console.error("로그아웃 실패:", error);
+    }
+  };
   return (
     <View style={styles.container}>
       <TitleImage width={200} height={125} />
@@ -18,7 +44,10 @@ export default function LoginScreen({ navigation }) {
       >
         <KakakoImage width={50} height={50} />
       </TouchableOpacity>
-
+      <View>
+        <Button title="카카오 로그인" onPress={handleLogin} />
+        <Button title="카카오 로그아웃" onPress={handleLogout} />
+      </View>
       <Text style={styles.buttonText}>카카오톡</Text>
     </View>
   );
